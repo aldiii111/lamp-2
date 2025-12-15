b80d57017281363b1ecb1f69070acb4d471275d0
aldiii <aldinissa093@gmail.com>
2025-12-15 23:06:30 +0700

    update js

---
+++ datanya.js
@@ -0,0 +1,28 @@
+function saklar() {
+let toggle1k = document.getElementById('toggle1k')
+let toggle2k = document.getElementById('toggle2k')
+let toggle3k = document.getElementById('toggle3k')
+let togglekall = document.getElementById('togglekall')
+
+let toggle1m = document.getElementById('toggle1m')
+let togglemall = document.getElementById('togglemall')
+
+let toggle1t = document.getElementById('toggle1t')
+let toggle2t = document.getElementById('toggle2t')
+let toggle3t = document.getElementById('toggle3t')
+let toggle4t = document.getElementById('toggle4t')
+let toggletall = document.getElementById('toggletall')
+
+let toggle1tt = document.getElementById('toggle1tt')
+let toggle2tt = document.getElementById('toggle2tt')
+let togglettall = document.getElementById('togglettall')
+
+let lampu1k = document.getElementById("lampu1k")
+
+    if (toggle1k.checked) {
+        lampu
+    }
+}
+
+
+
